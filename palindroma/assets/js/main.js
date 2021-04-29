1fe1970fd5c2a5baccceefc2746bc6949fec4717
Vituz <vtbattaglia@gmail.com>
2021-04-29 17:56:52 +0200

    Add correction to Palindroma

--- palindroma/assets/js/main.js
+++ palindroma/assets/js/main.js
@@ -1,17 +1,22 @@
-var play = true;s
+var play = true;
 
 while(play){
     
     // Chiedere all’utente di inserire una parola
     var parola_inserita = prompt("Inserisci una parola");
     
-    console.log(ifPalindroma(parola_inserita));
-    
-    
-    // console.log(palindroma);
-    
+    document.getElementById("palindroma").innerHTML = ifPalindroma(parola_inserita);
+        
     // Creare una funzione per capire se la parola inserita è palindroma
     
+    /**
+     * ### Controlla se una parola è palindroma
+     * Questa funzione accetta una stringa e la trasforma in un array, la inverte e la riunisce
+     * e controlla se la nuova parola è identica a quella di partenza.
+     * @param {string} parola - la parola da controllare
+     * @returns {string} restituisce se la parola è palindroma o no
+     */
+
     function ifPalindroma(parola){
     
         var parola = parola.toLowerCase();  
@@ -23,13 +28,15 @@ while(play){
         var palindroma = parola_reverse.join("");
     
         if (parola === palindroma){
+            result =`${parola} è una parola palindroma`;
             console.log(`${parola} è una parola palindroma`);
-            return true;
+            return result;
             
     
         } else {
+            result =`${parola} non è una parola palindroma`;
             console.log(`${parola} non è una parola palindroma`);
-            return false;
+            return result;
         }
     
     }
